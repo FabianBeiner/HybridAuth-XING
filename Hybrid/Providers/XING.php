3d8eeaddf26c310a20df145fc05e13188596d4af
Fabian Beiner <fb@fabianbeiner.de>
2015-02-26 16:11:34 +0100

    Changed the source code format, returning null instead of empty string, bumped up the version.

--- Hybrid/Providers/XING.php
+++ Hybrid/Providers/XING.php
@@ -1,17 +1,18 @@
 <?php
 /*!
 * HybridAuth
-* http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
-* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
+* http://hybridauth.sourceforge.net | https://github.com/hybridauth/hybridauth
+* (c) 2009-2015 HybridAuth authors | hybridauth.sourceforge.net/licenses.html
 */
 
 /**
  * XING.com Provider
  *
- * @author  Fabian Beiner <mail@fabian-beiner.de>
- * @version 1.0.1
+ * @author  Fabian Beiner <fb@fabianbeiner.de>
+ * @version 1.1.0
  */
-class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
+class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1
+{
     /**
      * Initialize.
      */
@@ -55,7 +56,7 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
             throw new Exception('Authentication failed! ' . $this->providerId . ' returned an invalid OAuth token.');
         }
 
-        $this->token('request_token'       , $aToken['oauth_token']);
+        $this->token('request_token', $aToken['oauth_token']);
         $this->token('request_token_secret', $aToken['oauth_token_secret']);
 
         // Redirect to the XING authorization URL.
@@ -92,7 +93,7 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
         $this->deleteToken('request_token_secret');
 
         // But store the access tokens for later usage.
-        $this->token('access_token',        $aToken['oauth_token']);
+        $this->token('access_token', $aToken['oauth_token']);
         $this->token('access_token_secret', $aToken['oauth_token_secret']);
 
         // Connection established!
@@ -100,10 +101,10 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
     }
 
     /**
-    * Gets the profile of the user who has granted access.
-    *
-    * @see https://dev.xing.com/docs/get/users/me
-    */
+     * Gets the profile of the user who has granted access.
+     *
+     * @see https://dev.xing.com/docs/get/users/me
+     */
     function getUserProfile() {
         $oResponse = $this->api->get('users/me');
 
@@ -125,13 +126,13 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
          *
          * @see  http://hybridauth.sourceforge.net/userguide/Profile_Data_User_Profile.html
          */
-        $this->user->profile->identifier    = (property_exists($oResponse, 'id'))           ? $oResponse->id           : '';
-        $this->user->profile->profileURL    = (property_exists($oResponse, 'permalink'))    ? $oResponse->permalink    : '';
+        $this->user->profile->identifier    = (property_exists($oResponse, 'id')) ? $oResponse->id : '';
+        $this->user->profile->profileURL    = (property_exists($oResponse, 'permalink')) ? $oResponse->permalink : '';
         $this->user->profile->displayName   = (property_exists($oResponse, 'display_name')) ? $oResponse->display_name : '';
-        $this->user->profile->description   = (property_exists($oResponse, 'interests'))    ? $oResponse->interests    : ''; // Not really a "description, but anyways …
-        $this->user->profile->firstName     = (property_exists($oResponse, 'first_name'))   ? $oResponse->first_name   : '';
-        $this->user->profile->lastName      = (property_exists($oResponse, 'last_name'))    ? $oResponse->last_name    : '';
-        $this->user->profile->gender        = (property_exists($oResponse, 'gender'))       ? $oResponse->gender       : '';
+        $this->user->profile->description   = (property_exists($oResponse, 'interests')) ? $oResponse->interests : ''; // Not really a "description, but anyways …
+        $this->user->profile->firstName     = (property_exists($oResponse, 'first_name')) ? $oResponse->first_name : '';
+        $this->user->profile->lastName      = (property_exists($oResponse, 'last_name')) ? $oResponse->last_name : '';
+        $this->user->profile->gender        = (property_exists($oResponse, 'gender')) ? $oResponse->gender : '';
         $this->user->profile->emailVerified = (property_exists($oResponse, 'active_email')) ? $oResponse->active_email : '';
 
         // My own priority: Homepage, blog, other, something else.
@@ -181,19 +182,19 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
             $oAddress = $oResponse->private_address;
         }
         if (null !== $oAddress) {
-            $this->user->profile->phone   = (property_exists($oAddress, 'phone'))        ? $oAddress->phone        : '';
-            $this->user->profile->address = (property_exists($oAddress, 'street'))       ? $oAddress->street       : '';
-            $this->user->profile->country = (property_exists($oAddress, 'country'))      ? $oAddress->country      : '';
-            $this->user->profile->region  = (property_exists($oAddress, 'province'))     ? $oAddress->province     : '';
-            $this->user->profile->city    = (property_exists($oAddress, 'city'))         ? $oAddress->city         : '';
-            $this->user->profile->zip     = (property_exists($oAddress, 'zip_code'))     ? $oAddress->zip_code     : '';
-            $this->user->profile->email   = (property_exists($oAddress, 'email'))        ? $oAddress->email        : '';
+            $this->user->profile->phone   = (property_exists($oAddress, 'phone')) ? $oAddress->phone : '';
+            $this->user->profile->address = (property_exists($oAddress, 'street')) ? $oAddress->street : '';
+            $this->user->profile->country = (property_exists($oAddress, 'country')) ? $oAddress->country : '';
+            $this->user->profile->region  = (property_exists($oAddress, 'province')) ? $oAddress->province : '';
+            $this->user->profile->city    = (property_exists($oAddress, 'city')) ? $oAddress->city : '';
+            $this->user->profile->zip     = (property_exists($oAddress, 'zip_code')) ? $oAddress->zip_code : '';
+            $this->user->profile->email   = (property_exists($oAddress, 'email')) ? $oAddress->email : '';
             if (null === $this->user->profile->language) {
                 $this->user->profile->language = (property_exists($oAddress, 'country')) ? $oAddress->country : '';
             }
             // The following two are actually not part of the normalized user profile structure used by HybridAuth...
-            $this->user->profile->mobile  = (property_exists($oAddress, 'mobile_phone')) ? $oAddress->mobile_phone : '';
-            $this->user->profile->fax     = (property_exists($oAddress, 'fax'))          ? $oAddress->fax          : '';
+            $this->user->profile->mobile = (property_exists($oAddress, 'mobile_phone')) ? $oAddress->mobile_phone : '';
+            $this->user->profile->fax    = (property_exists($oAddress, 'fax')) ? $oAddress->fax : '';
         }
 
         return $this->user->profile;
@@ -205,10 +206,8 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
      * @see http://hybridauth.sourceforge.net/userguide/Profile_Data_User_Status.html
      */
     function setUserStatus($sMessage) {
-        $aParameters = array(
-            'oauth_token' => $this->token('access_token')
-           ,'id'          => 'me'
-        );
+        $aParameters = array('oauth_token' => $this->token('access_token'),
+                             'id'          => 'me');
 
         // German network, there will probably be Umlauts somewhere. :)
         mb_internal_encoding('UTF-8');
@@ -236,9 +235,9 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
             elseif ($this->api->http_code === 404) {
                 throw new Exception('The user "' . $aParameters['id'] . '" was not found.');
             }
+
             return false;
-        }
-        catch(Exception $e) {
+        } catch (Exception $e) {
             throw new Exception('Could not update the status. ' . $this->providerId . ' returned an error: ' . $e . '.');
         }
     }
@@ -262,15 +261,14 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
                 throw new Exception('User Contact request failed! ' . $this->providerId . ' API returned an error: invalid response.');
             }
 
-            $oTotal    = $oResponse->contacts->users;
-            $iTotal    = $oResponse->contacts->total;
+            $oTotal = $oResponse->contacts->users;
+            $iTotal = $oResponse->contacts->total;
 
             for ($i = 100; $i <= $iTotal; $i = $i + 100) {
                 $oResponse = $this->api->get('users/me/contacts?limit=100&user_fields=id,display_name,permalink,web_profiles,photo_urls,display_name,interests,active_email&offset=' . $i);
                 $oTotal    = array_merge($oTotal, $oResponse->contacts->users);
             }
-        }
-        catch(Exception $e) {
+        } catch (Exception $e) {
             throw new Exception('Could not fetch contacts. ' . $this->providerId . ' returned an error: ' . $e . '.', $e->getCode());
         }
 
@@ -281,12 +279,12 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1 {
 
         // Create the contacts array.
         $aContacts = array();
-        foreach($oTotal as $aTitle) {
-            $oContact = new Hybrid_User_Contact();
-            $oContact->identifier  = (property_exists($aTitle, 'id'))           ? $aTitle->id           : '';
-            $oContact->profileURL  = (property_exists($aTitle, 'permalink'))    ? $aTitle->permalink    : '';
+        foreach ($oTotal as $aTitle) {
+            $oContact              = new Hybrid_User_Contact();
+            $oContact->identifier  = (property_exists($aTitle, 'id')) ? $aTitle->id : '';
+            $oContact->profileURL  = (property_exists($aTitle, 'permalink')) ? $aTitle->permalink : '';
             $oContact->displayName = (property_exists($aTitle, 'display_name')) ? $aTitle->display_name : '';
-            $oContact->description = (property_exists($aTitle, 'interests'))    ? $aTitle->interests    : '';
+            $oContact->description = (property_exists($aTitle, 'interests')) ? $aTitle->interests : '';
             $oContact->email       = (property_exists($aTitle, 'active_email')) ? $aTitle->active_email : '';
 
             // My own priority: Homepage, blog, other, something else.
