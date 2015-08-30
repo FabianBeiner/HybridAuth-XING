cc797770a32f16db867b5eaeb37ad22fbd89b431
dawez <dawez1@gmail.com>
2015-08-30 21:35:03 +0200

    added function to search for emails

--- Hybrid/Providers/XING.php
+++ Hybrid/Providers/XING.php
@@ -246,6 +246,8 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1
      * Load user contacts.
      *
      * @see http://hybridauth.sourceforge.net/userguide/Profile_Data_User_Contacts.html
+     * @return Hybrid_User_Contact[]
+     * @throws Exception
      */
     function getUserContacts() {
         try {
@@ -344,4 +346,165 @@ class Hybrid_Providers_XING extends Hybrid_Provider_Model_OAuth1
         }
     }
 
+    /**
+     * Find users by given email
+     *
+     * @see https://dev.xing.com/docs/get/users/find_by_emails
+     *
+     * @param array $emails the list of emails that will be searched in XING
+     * @param boolean $isUserExisting collect only user that have an accessible XING profile
+     * @return XingUser[] the associative array with emails as key
+     * @throws Exception
+     */
+    public function findUsersByEmail( $emails, $isUserExisting = true )
+    {
+        $user_fields_string = XingUser::getApiRequestFields();
+
+        $aParameters = array(
+            'oauth_token' => $this->token( 'access_token' ),
+            'user_fields' => $user_fields_string,
+        );
+
+        $found_users = array();
+        //each email seach request has a limit of 100 emails
+        $all_emails_chunks = array_chunk( $emails, 100 );
+        foreach ($all_emails_chunks as $single_emails_chunk) {
+            $aParameters[ 'emails' ] = implode( ',', $single_emails_chunk );
+            $oResponse = $this->api->get( 'users/find_by_emails', $aParameters );
+
+            $this->verifyResponse( 'find_by_emails', $this->api->http_code, $oResponse );
+
+            // parse response
+            foreach ($oResponse->results->items as $item) {
+                $user_email = $item->email;
+                $user = array();
+                if (null !== $item->user) {
+                    // valid user
+                    if (property_exists( $item->user, 'id' ) && $item->user->id != null) {
+                        // if id is null then the user is inactive or something wrong anyway
+                        $user = new XingUser( $item->user );
+                    }
+                }
+
+                // filter only found users if requested
+                if (!$isUserExisting || ( $isUserExisting && ( count( $user ) > 0 ) )) {
+                    $found_users[ $user_email ] = $user;
+                }
+            }
+        }
+
+        return $found_users;
+    }
+
+    private function verifyResponse( $requestName, $http_code, $oResponse )
+    {
+        // The HTTP status code needs to be 200 here. If it's not, something is wrong.
+        if ($this->api->http_code !== 200) {
+            throw new Exception(
+                $requestName . ' request failed! ' . $this->providerId . ' API returned an error: ' . $this->errorMessageByStatus( $http_code ) . '.'
+            );
+        }
+
+        // We should have an object by now.
+        if (!is_object( $oResponse )) {
+            throw new Exception( $requestName . ' request failed! ' . $this->providerId . ' API returned an error: invalid response.' );
+        }
+
+        return true;
+    }
+}
+
+/**
+ * XingUser - basic XING user profile
+ *
+ * This is based on the standard Hybrid_User_Contact with some more specific fields
+ */
+class XingUser extends Hybrid_User_Contact
+{
+
+    // maps Hybrid_User_Contact to its relative XING field ids
+    private static $xingUser_xingapi_fields_map = array(
+        'identifier' => 'id',
+        // priority ordered, they are parsed in order. If there is nothing also in web_profiles/other, it takes any web_profiles/ existing
+        'webSiteURL' => array( 'web_profiles/homepage', 'web_profiles/blog', 'web_profiles/other', 'web_profiles/*' ),
+        'profileURL' => 'permalink',
+        // uses the 'large' value from the photo_urls as the user pic
+        'photoURL' => array( 'photo_urls/large' ),
+        'displayName' => 'display_name',
+        'description' => 'interests',
+        'email' => 'active_email',
+        'firstName' => 'first_name',
+        'lastName' => 'last_name',
+        'employmentStatus' => 'employment_status',
+        'gender' => 'gender',
+    );
+
+    // extra attributes that are not included in the Hybrid_User_Contact
+    public $firstName = null;
+    public $lastName = null;
+    public $employmentStatus = null;
+    // gender values seems not to be reliable
+    public $gender = null;
+
+    /**
+     * XINGUser constructor.
+     *
+     * Create a XING user using with the data coming from the API response
+     * @param  stdClass $oResponse the response coming from XING api request
+     * @throws Exception
+     */
+    public function __construct( $oResponse )
+    {
+        foreach (self::$xingUser_xingapi_fields_map as $classFieldName => $apiFieldName) {
+            if (!in_array( $classFieldName, array_keys( get_object_vars( $this ) ) )) {
+                throw new Exception( "Cannot find class property [ $classFieldName ]" );
+            }
+            if (is_array( $apiFieldName )) {
+                // if there are multiple elements, there is a preference order in which they are assigned
+                // TODO at the moment just 2 levels but should be recursive to parse also more complex nested fields
+                foreach ($apiFieldName as $apiFieldNameItem) {
+                    list ( $apiFieldNameItemParent, $apiFieldNameItemChild ) = explode( '/', $apiFieldNameItem );
+                    if (!strpos( $apiFieldNameItem, '/' )) {
+                        throw new Exception( "Invalid nested property defined [ $classFieldName => $apiFieldNameItem ] " );
+                    }
+
+                    if (property_exists( $oResponse, $apiFieldNameItemParent )) {
+                        if (( $apiFieldNameItemChild ==='*' ) && ( count( get_object_vars( $oResponse->$apiFieldNameItemParent ) ) > 0 )) {
+                            // anything is valid then
+                            foreach (array_values( get_object_vars( $oResponse->$apiFieldNameItemParent ) ) as $itemChildValue) {
+                                if ($itemChildValue != null) {
+                                    $this->$classFieldName = $itemChildValue;
+                                    break;
+                                }
+                            }
+                        } else {
+                            if (property_exists( $oResponse->$apiFieldNameItemParent, $apiFieldNameItemChild )) {
+                                $this->$classFieldName = $oResponse->$apiFieldNameItemParent->$apiFieldNameItemChild;
+                                // in case we have multiple elements such as in web_profiles/ , we take the fist match
+                                break;
+                            }
+                        }
+                    }
+                }
+            } else {
+                // simple property
+                $this->$classFieldName = $oResponse->$apiFieldName;
+            }
+        }
+    }
+
+    public static function getApiRequestFields()
+    {
+        $xingApiFields = array_values( self::$xingUser_xingapi_fields_map );
+        $apiFields = array();
+        foreach ($xingApiFields as $xingApiField) {
+            if (is_array( $xingApiField )) {
+                // nested property
+                $xingApiField = explode( '/', $xingApiField[ 0 ] )[ 0 ];
+            }
+            $apiFields[] = $xingApiField;
+        }
+
+        return implode( ',', $apiFields );
+    }
 }
